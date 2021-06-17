69900f75d2e7412260b65d1e12548ceaae1a02c9
etamar <etamar.levy@gmail.com>
2021-06-17 17:25:35 -0400

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import About from './components/About'
+import Home from './components/Home'
 
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
