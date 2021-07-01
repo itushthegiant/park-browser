2fbd1d63184d09d1983ff9258b06faa1b91290b3
etamar <etamar.levy@gmail.com>
2021-07-01 08:56:19 -0400

    changing triplog

---
+++ src/components/GuestForm.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+
+export default class GuestForm extends Component {
+
+    state = {
+        name: '',
+        createdAt: '',
+        content: '',
+    }
+
+
+    handleOnChange = (event) => {
+        const  { name, value } = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        fetch('http://localhost:5000/triplog', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                guestName: this.state.name,
+                createdAt: '',
+                content: this.state.content
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => this.props.addNewLog(data))
+    }
+
+
+
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.handleSubmit}>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Name" name='name' onChange={this.handleOnChange} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Tell us about your experience</Form.Label>
+                            <Form.Control as="textarea" name='content' rows={7} placeholder="Your experience..." onChange={this.handleOnChange} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+            </div>
+        )
+    }
+}
--- src/components/Triplog.js
+++ src/components/Triplog.js
@@ -1,45 +1,19 @@
 import React, { Component } from 'react'
-import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
+import GuestForm from './GuestForm'
 
 
 export default class Triplog extends Component {
 
     state = {
-        name: '',
-        createdAt: '',
-        content: '',
+        logs: []
     }
 
-
-    handleNameChange = (event) => {
+    
+    addNewLog = (log) => {
         this.setState({
-            name: event.target.value
+            logs: [...this.state.logs, log]
         })
-    }
-
-
-    handleContentChange = (event) => {
-        this.setState({
-            content: event.target.value
-        })
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault()
-        fetch('http://localhost:5000/triplog', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                guestName: this.state.name,
-                content: this.state.content
-            })
-        })
-        .then(resp => console.log(resp))
     }
 
 
@@ -52,19 +26,11 @@ export default class Triplog extends Component {
                     Triplog
                 </h1>
                 <Container>
-                    <Form onSubmit={this.handleSubmit}>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control type="text" placeholder="Name" onChange={this.handleNameChange} />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Tell us about your experience</Form.Label>
-                            <Form.Control as="textarea" rows={10} placeholder="Your experience..." onChange={this.handleContentChange} />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
+                    <GuestForm addNewLog={this.addNewLog} />
+                </Container>
+
+                <Container>
+
                 </Container>
             </div>
         )
